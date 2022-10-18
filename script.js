32df0682e1f34f044ff329ff6c31b7410ce868a0
Beppe94 <giuseppe.monorchio94@hotmail.it>
2022-10-18 22:23:15 +0200

    added regex to password funtion and better css

--- script.js
+++ script.js
@@ -3,19 +3,33 @@ function check() {
     const password = document.getElementById('password')
     const confPassword = document.getElementById('confirmPassword')
     const pwText = document.getElementById('pwText')
-    const form = document.querySelector('form')
+    const upperCase = document.getElementById('upperCase')
     const num = document.getElementById('num')
 
     if(password.value.match(/[0-9]/)) {
+        num.textContent = 'At least one number ✓'
         num.style.color = 'green'
     } else {
+        num.textContent = 'At least one number 🗴'
         num.style.color = 'red'
     }
 
-    if(password.value != confPassword.value) {
-        pwText.style.color = 'red'
-    } else {
-        pwText.style.color = 'green'
+    if(password.value.match(/[A-Z]/)) {
+        upperCase.textContent = 'At least one upper case ✓'
+        upperCase.style.color = 'green'
+    } else  {
+        upperCase.textContent = 'At least one upper case 🗴' 
+        upperCase.style.color = 'red'
     }
+
+    valid(password, confPassword, pwText)
 }
 
+
+function valid(pw1, pw2, text) {
+    if(pw1.value != pw2.value) {
+        text.style.color = 'red'
+    } else {
+        text.style.color = 'green'
+    }
+}
