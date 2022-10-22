3322753d08f149b0b4c9c917964da8155396861c
Beppe94 <giuseppe.monorchio94@hotmail.it>
2022-10-22 20:49:10 +0200

    added event listener on form to prevent user sign up with wrong infos

--- script.js
+++ script.js
@@ -1,35 +1,72 @@
+const form = document.getElementById('form');
+const password = document.getElementById('password');
+const confPassword = document.getElementById('confirmPassword');
+const pwText = document.getElementById('pwText');
+const upperCase = document.getElementById('upperCase');
+const num = document.getElementById('num');
+const pwLenght = document.getElementById('pwLength')
+const submitBtn = document.getElementById('submit')
+
+
+
+
+
+
+form.addEventListener('submit', (e) => {
+    
+    
+    if(valid() && check()) {
+        form.submit();
+    } else {
+        e.preventDefault()
+    }
+    
+});
+
 function check() {
 
-    const password = document.getElementById('password')
-    const confPassword = document.getElementById('confirmPassword')
-    const pwText = document.getElementById('pwText')
-    const upperCase = document.getElementById('upperCase')
-    const num = document.getElementById('num')
+    let hasErrors = false 
 
     if(password.value.match(/[0-9]/)) {
         num.textContent = 'At least one number ✓'
         num.style.color = 'green'
+        hasErrors = true
     } else {
         num.textContent = 'At least one number 🗴'
         num.style.color = 'red'
+        hasErrors = false
     }
 
     if(password.value.match(/[A-Z]/)) {
         upperCase.textContent = 'At least one upper case ✓'
         upperCase.style.color = 'green'
-    } else  {
+        hasErrors = true
+    } else {
         upperCase.textContent = 'At least one upper case 🗴' 
         upperCase.style.color = 'red'
+        hasErrors = false;
     }
 
-    valid(password, confPassword, pwText)
+    if(password.value.length < 5) {
+        pwLenght.textContent = 'At least 5 characters ✓'
+        pwLenght.style.color = 'red'
+        hasErrors = false;
+    } else {
+        pwLenght.textContent = 'At least 5 characters 🗴'
+        pwLenght.style.color = 'green'
+        hasErrors = true;
+    }
+    
+    return hasErrors;
 }
 
 
-function valid(pw1, pw2, text) {
-    if(pw1.value != pw2.value) {
-        text.style.color = 'red'
+function valid() {
+    if(password.value != confPassword.value) {
+        pwText.style.color = 'red'
+        return false
     } else {
-        text.style.color = 'green'
+        pwText.style.color = 'green'
+        return true
     }
 }
